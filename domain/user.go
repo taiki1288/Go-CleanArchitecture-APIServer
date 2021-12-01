4d66cca09654c0b8b240a8cc5ba23c000f971f03
taiki yamashita <taikity0808@gmail.com>
2021-12-01 20:33:51 +0900

    feature: domain層のUser構造体を定義

---
+++ domain/user.go
@@ -0,0 +1,11 @@
+package domain
+
+import "time"
+
+type User struct {
+	ID        int       `json:"id" gorm:"primary_key"`
+	Name      string    `json:"name"`
+	Age       int       `json:"age"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+}
