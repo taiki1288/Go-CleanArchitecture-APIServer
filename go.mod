c3c79bb5335916f22a0a3f540a86c7c5773dcc4a
taiki yamashita <taikity0808@gmail.com>
2021-12-01 22:49:33 +0900

    feature: NewSqlHandler関数を定義

--- go.mod
+++ go.mod
@@ -3,7 +3,12 @@ module Go-CleanArchitecture-APIServer
 go 1.17
 
 require (
-	github.com/go-sql-driver/mysql v1.5.0 // indirect
-	github.com/jinzhu/gorm v1.9.16 // indirect
+	gorm.io/driver/mysql v1.2.1
+	gorm.io/gorm v1.22.4
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.3 // indirect
 )
--- infrastructure/sqlhandler.go
+++ infrastructure/sqlhandler.go
@@ -1,7 +1,22 @@
 package infrastructure
 
-import "github.com/jinzhu/gorm"
+import (
+
+	"gorm.io/gorm"
+	"gorm.io/driver/mysql"
+) 
 
 type SqlHandler struct {
 	db *gorm.DB
+}
+
+func NewSqlHandler() database.SqlHandler {
+	dsn := "user:pass@tcp(127.0.0.1:3306)/Go-CleanArchitecture-APIServer?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err.Error)
+	}
+	sqlhandler := new(SqlHandler)
+	sqlhandler.db = db 
+	return sqlhandler
 }
