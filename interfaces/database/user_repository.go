bb9f5687fd9ede0854f6f5b082f8bfc19b71b23f
taiki yamashita <taikity0808@gmail.com>
2021-12-03 00:42:51 +0900

    feature: interfaces/database層のStoreメソッドを定義

--- interfaces/database/user_repository.go
+++ interfaces/database/user_repository.go
@@ -1,5 +1,15 @@
 package database
 
+import "Go-CleanArchitecture-APIServer/domain"
+
 type UserRepository struct {
 	SqlHandler
+}
+
+func (db *UserRepository) Store(u domain.User) (user domain.User, err error) {
+	if err = db.Create(&u).Error; err != nil {
+		return
+	}
+	user = u
+	return
 }
