485efb0fb60678d856598e6cf3969d6cc7088e69
taiki yamashita <taikity0808@gmail.com>
2021-12-03 16:47:31 +0900

    Add: Comment

--- usecase/user_interactor.go
+++ usecase/user_interactor.go
@@ -8,6 +8,7 @@ type UserInteractor struct {
 	UserRepository UserRepository
 }
 
+// User情報を作成して返すメソッド
 func (interactor *UserInteractor) Add(u domain.User) (user domain.User, err error) {
 	user, err = interactor.UserRepository.Store(u)
 	return
@@ -18,6 +19,7 @@ func (interactor *UserInteractor) UserById(id int) (user domain.User, err error)
 	return
 }
 
+// ユーザー一覧を返すメソッド
 func (interactor *UserInteractor) Users() (user domain.Users, err error) {
 	user, err = interactor.UserRepository.FindAll()
 	return
