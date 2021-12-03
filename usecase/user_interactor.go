c6931ee2afc02809bb5652940571b0d30610b82e
taiki yamashita <taikity0808@gmail.com>
2021-12-03 16:31:49 +0900

    feature: usecase層のAddメソッドを定義

--- usecase/user_interactor.go
+++ usecase/user_interactor.go
@@ -1,7 +1,15 @@
 package usecase
 
-import "Go-CleanArchitecture-APIServer/domain"
+import (
+	"Go-CleanArchitecture-APIServer/domain"
+	"fmt"
+)
 
 type UserInteractor struct {
 	UserRepository UserRepository
+}
+
+func (interactor *UserInteractor) Add(u domain.User) (user domain.User, err error) {
+	user, err = interactor.UserRepository.Store(u)
+	return
 }
