368fac93f7d04df1c1b9f4add27a556b2b2d4e5f
taiki yamashita <taikity0808@gmail.com>
2021-12-03 13:18:32 +0900

    Add: Comment

--- interfaces/database/sql_handler.go
+++ interfaces/database/sql_handler.go
@@ -2,6 +2,7 @@ package database
 
 import "gorm.io/gorm"
 
+// database/user_repository.goでこれを呼び出す。(DIP: 依存関係逆転の原則)
 type SqlHandler interface {
 	Create(interface{}) *gorm.DB
 	Find(interface{}, ...interface{}) *gorm.DB
