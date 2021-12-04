305f1011db9670b2e6c86a881552cd73aea82b5f
taiki yamashita <taikity0808@gmail.com>
2021-12-04 23:15:00 +0900

    feature: interfaces/controller層のCreateUserメソッドを定義

--- interfaces/controllers/user_controller.go
+++ interfaces/controllers/user_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"Go-CleanArchitecture-APIServer/domain"
 	"Go-CleanArchitecture-APIServer/interfaces/database"
 	"Go-CleanArchitecture-APIServer/usecase"
 )
@@ -17,4 +18,15 @@ func NewUserController(sqlhandler database.SqlHandler) *UserController {
 			},
 		},
 	}
+}
+
+func (controller *UserController) CreateUser(c Context) (err error) {
+	u := domain.User{}
+	c.Bind(&u)
+	user, err := controller.Interactor.Add(u)
+	if err != nil {
+		c.JSON(500, NewError(err))
+	}
+	c.JSON(201, user)
+	return
 }
