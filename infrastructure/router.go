6ef0725db792091ff96fa5f552dd8c3a89cd9c86
taiki yamashita <taikity0808@gmail.com>
2021-12-05 21:36:54 +0900

    feature: infrastructure層のInit関数を定義

---
+++ infrastructure/router.go
@@ -0,0 +1,28 @@
+package infrastructure
+
+import (
+	"Go-CleanArchitecture-APIServer/interfaces/controllers"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func Init() {
+	e := echo.New()
+
+	userController := controllers.NewUserController(NewSqlHandler())
+
+	// middleware.Loggerはアクセスログのようなリクエスト単位のログを出力する。
+	e.Use(middleware.Logger())
+	// middleware.Recoverはアプリケーションのどこかで予期せずにpanicを起こしてしまっても、サーバは落とさずにエラーレスポンスを返せるようにリカバリーする。
+	e.Use(middleware.Recover())
+
+	e.POST("/users", func(c echo.Context) error { return userController.CreateUser(c) })
+	e.GET("/users", func(c echo.Context) error { return userController.GetUsers(c) })
+	e.GET("/users/:id", func(c echo.Context) error { return userController.GetUser(c) })
+	e.PUT("/users/:id", func(c echo.Context) error { return userController.UpdateUser(c) })
+	e.DELETE("/users/:id", func(c echo.Context) error { return userController.DeleteUser(c) })
+
+	// サーバー起動
+	e.Logger.Fatal(e.Start(":1323"))
+}
